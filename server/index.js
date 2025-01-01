9753dbe03e668eb792f76acee2a119fa798ce533
Sthera30 <tinisthera@gmail.com>
2025-01-02 00:08:34 +0200

    updated backend

--- server/index.js
+++ server/index.js
@@ -26,7 +26,6 @@ app.use(cookieParser())
 
 const corsOptions = {
     origin: [
-        "http://localhost:5173", 
         "https://fullstack-kwanosportsclub-app-frontend.onrender.com" 
     ],
   methods: ["GET", "POST", "PUT", "DELETE"], 
